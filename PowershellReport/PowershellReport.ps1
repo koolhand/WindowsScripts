1bce7ba83d5f23512d8444ff9927f96fdbd39e54
Luke Morey <lukemorey@gmail.com>
2021-05-30 20:56:29 +1000

    PowershellReport basic

---
+++ PowershellReport/PowershellReport.ps1
@@ -0,0 +1,34 @@
+$htmlTemplateTop = @"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>{0}</title>
+  </head>
+  <body>
+  <h1>{1}</h1>
+  {2}
+"@
+
+$htmlTemplateBottom = @"
+</body>
+</html>
+"@
+
+$htmlDescriptionTemplate = @"
+<h2>{0}</h2>
+<p>{1}</p>
+"@
+
+$htmlTitle = "The Title"
+$htmlIntro = "<p>A bunch of useful reports from {0}</p>" -f (Get-Date -Format "dddd d MMM yyyy, h:mm:ss tt")
+
+($htmlDescriptionTemplate -f "Services","A list of Windows services.")      | Out-File './html/temp/1.html'
+Get-Service | ConvertTo-Html  -Fragment -Property DisplayName, Name, Status | Out-File './html/temp/1.html' -Append
+
+($htmlDescriptionTemplate -f "Modules","A list of PowerShell services.")    | Out-File './html/temp/2.html'
+Get-Module  | ConvertTo-Html  -Fragment -Property Name, Version             | Out-File './html/temp/2.html' -Append
+
+($htmlTemplateTop -f $htmlTitle, $htmlTitle, $htmlIntro), (Get-Content './html/temp/*.htm*'), $htmlTemplateBottom | Set-Content './html/output/report.html'
+
+Invoke-Item './html/output/report.html'
