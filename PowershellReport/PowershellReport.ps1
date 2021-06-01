e646d678015fe0115056159ded6ee71c85cf46ea
Luke Morey <Luke.Morey@au.kwm.com>
2021-06-01 14:00:31 +1000

    insert basic CSS into HTML

--- PowershellReport/PowershellReport.ps1
+++ PowershellReport/PowershellReport.ps1
@@ -4,9 +4,12 @@ $htmlTemplateTop = @"
   <head>
     <meta charset="utf-8" />
     <title>{0}</title>
+    <style>
+    {1}
+    </style>
   </head>
   <body>
-  <h1>{1}</h1>
+  <h1>{0}</h1>
   {2}
 "@
 
@@ -22,6 +25,7 @@ $htmlDescriptionTemplate = @"
 
 $htmlTitle = "The Title"
 $htmlIntro = "<p>A bunch of useful reports from {0}</p>" -f (Get-Date -Format "dddd d MMM yyyy, h:mm:ss tt")
+$htmlCss = (Get-Content -Path "./html/gutenberg-min.css" | Out-String)
 
 ($htmlDescriptionTemplate -f "Services","A list of Windows services.")      | Out-File './html/temp/1.html'
 Get-Service | ConvertTo-Html  -Fragment -Property DisplayName, Name, Status | Out-File './html/temp/1.html' -Append
@@ -29,6 +33,6 @@ Get-Service | ConvertTo-Html  -Fragment -Property DisplayName, Name, Status | Ou
 ($htmlDescriptionTemplate -f "Modules","A list of PowerShell services.")    | Out-File './html/temp/2.html'
 Get-Module  | ConvertTo-Html  -Fragment -Property Name, Version             | Out-File './html/temp/2.html' -Append
 
-($htmlTemplateTop -f $htmlTitle, $htmlTitle, $htmlIntro), (Get-Content './html/temp/*.htm*'), $htmlTemplateBottom | Set-Content './html/output/report.html'
+($htmlTemplateTop -f $htmlTitle, $htmlCss, $htmlIntro), (Get-Content './html/temp/*.htm*'), $htmlTemplateBottom | Set-Content './html/output/report.html'
 
 Invoke-Item './html/output/report.html'
